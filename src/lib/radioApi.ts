9d72331ff3edf6151cd9b262a4e5593be14913bf
Yevhenii <jenmikitenko@gmail.com>
2024-10-01 09:15:26 +0300

    fix mix content issues

--- src/lib/radioApi.ts
+++ src/lib/radioApi.ts
@@ -1,4 +1,5 @@
 import { RadioBrowserApi } from "radio-browser-api";
 
 const radio_api = new RadioBrowserApi("My Radio App");
+ radio_api.setBaseUrl('https://at1.api.radio-browser.info') 
 export default radio_api;
